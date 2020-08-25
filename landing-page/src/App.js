f94ed998cac8c3d765f25abd5ac4418ad605362c
Nazim Uddin <umnazim@gmail.com>
2020-08-24 23:02:30 -0700

    note app added

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Footer from "./components/Footer.jsx";
 import Media from "./components/Media.jsx";
@@ -9,9 +9,29 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from "./components/NavigationBar.jsx";
 
+import Note from "./components/Note";
+
+import CreateArea from "./components/CreateArea";
+
 import "./App.css";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div className="App">
       <React.Fragment>
@@ -19,16 +39,33 @@ function App() {
           <NavigationBar />
           <Quotes />
           <h3>Links of most frequently used websites</h3>
+
           <div class="media">
             <div class="news">
               <Media />
             </div>
+
             <div class="learning">
               <Learning />
             </div>
+            <div class = "note">
+              <CreateArea onAdd={addNote} />
+              {notes.map((noteItem, index) => {
+                return (
+                  <Note
+                    key={index}
+                    id={index}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    onDelete={deleteNote}
+                  />
+                );
+              })}
+              ;
+            </div>
           </div>
 
-          <Footer fixed="bottom" />
+          <Footer class="footer" />
         </Router>
       </React.Fragment>
     </div>
